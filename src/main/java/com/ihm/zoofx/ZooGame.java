6c1d320544f87a4ecce721b885e49bffd4c868f7
Fasory <72564803+Fasory@users.noreply.github.com>
2022-05-19 12:36:03 +0200

    Set icon

--- src/main/java/com/ihm/zoofx/ZooGame.java
+++ src/main/java/com/ihm/zoofx/ZooGame.java
@@ -3,6 +3,7 @@ package com.ihm.zoofx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class ZooGame extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ZooGame.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("ZooGame");
+        stage.getIcons().add(new Image(String.valueOf(ZooGame.class.getResource("icone.png"))));
         stage.setScene(scene);
         stage.show();
     }
