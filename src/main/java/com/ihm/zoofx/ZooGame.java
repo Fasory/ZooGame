d4f80649f4b13f51ec3c703f4a3184840ca3e045
Fasory <72564803+Fasory@users.noreply.github.com>
2022-05-23 15:33:03 +0200

    First version of main menu

--- src/main/java/com/ihm/zoofx/ZooGame.java
+++ src/main/java/com/ihm/zoofx/ZooGame.java
@@ -4,14 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class ZooGame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ZooGame.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ZooGame.class.getResource("layouts/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1600, 900);                                              // résolution 16:9
         stage.getIcons().add(new Image(String.valueOf(ZooGame.class.getResource("img/icone.png"))));
         stage.setTitle("ZooGame");
